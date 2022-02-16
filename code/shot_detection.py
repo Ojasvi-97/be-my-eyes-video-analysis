d46916c19b518883ff9776b796428002fca084da
Ojasvi Bhalerao <ojubhalerao@gmail.com>
2022-02-16 16:36:38 -0700

    new shot detection code

--- code/shot_detection.py
+++ code/shot_detection.py
@@ -1,56 +1,36 @@
-import argparse
-import imutils
-import cv2
-import os
+
+from scenedetect import VideoManager
+from scenedetect import SceneManager
+
+# For content-aware scene detection:
+from scenedetect.detectors import ContentDetector
 
 
 class ShotDetector:
     def __init__(self, video_path):
         self.video_path = video_path
 
-    def detect_shot_boundary(self):
-        fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
 
-        captured, total, frames = False, 0, 0
-        count = 0
+    def find_scenes(self, threshold=10.0):
+        # Create our video & scene managers, then add the detector.
+        video_manager = VideoManager([self.video_path])
+        scene_manager = SceneManager()
+        scene_manager.add_detector(
+            ContentDetector(threshold=threshold))
 
-        vs = cv2.VideoCapture(self.video_path)
-        (W, H) = (None, None)
+        # Improve processing speed by downscaling before processing.
+        video_manager.set_downscale_factor()
 
-        while True:
-            (grabbed, frame) = vs.read()
-
-            if frame is None:
-                break
-
-            orig = frame.copy()
-            frame = imutils.resize(frame, width=600)
-            mask = fgbg.apply(frame)
-
-            mask = cv2.erode(mask, None, iterations=2)
-            mask = cv2.dilate(mask, None, iterations=2)
-
-            if W is None or H is None:
-                (H, W) = mask.shape[:2]
-
-            p = (cv2.countNonZero(mask) / float(W * H)) * 100
-
-            if p < args["min_percent"] and not captured and frames > args["warmup"]:
-
-                cv2.imwrite("Captured" + str(total), frame)
-                captured = True
-
-                filename = "{}.png".format(total)
-                path = os.path.sep.join(["shot_detection_results", filename])
-                total += 1
-                # save the  *original, high resolution* frame to disk
-                print("[INFO] saving {}".format(path))
-                cv2.imwrite(path, orig)
-
-            elif captured and p >= args["max_percent"]:
-                captured = False
-            frames += 1
-
+        # Start the video manager and perform the scene detection.
+        video_manager.start()
+        scene_manager.detect_scenes(frame_source=video_manager)
+        # scene_manager.save-images()
+        # Each returned scene is a tuple of the (start, end) timecode.
+        return scene_manager.get_scene_list()
 
 def main():
-    ShotDetector(video_path="videos/vid1.mp4").detect_shot_boundary()
+    scene_list = ShotDetector(video_path="videos/vid2.mp4").find_scenes()
+    print(scene_list)
+
+if __name__ == "__main__":
+    main()
